9bc689c0cc24523cc5425860c6a9d9417c8d2cdb
believelove <believelovehfer@gmail.com>
2019-11-17 19:35:02 -0600

    ocr

--- lib/lstm/utils/gen.py
+++ lib/lstm/utils/gen.py
@@ -10,6 +10,7 @@ import scipy.optimize
 import matplotlib.pyplot as plt
 import matplotlib.patches as Patches
 #from shapely.geometry import Polygon
+sys.path.append("/mnt/Samsung860/Wenrui/lstm_ctc_ocr/")
 from lib.utils.data_util import GeneratorEnqueuer
 from lib.lstm.config import cfg,get_encode_decode_dict
 import tensorflow as tf
